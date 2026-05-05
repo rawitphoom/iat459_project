f474a5fc5ad8335d6dd343467fd8d924f5b0615a
Rawitphoom Kiatthitinan <rawitphoom1234@hotmail.com>
2026-05-05 11:52:41 +0700

    ipad size update

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -584,10 +584,11 @@ export default function LandingPage() {
   useEffect(() => {
     const onResize = () => {
       const w = window.innerWidth;
-      if (w < 480) setRadius(200);
-      else if (w < 768) setRadius(260);
-      else if (w < 1024) setRadius(360);
-      else setRadius(485);
+      if (w < 480) setRadius(170);
+      else if (w < 768) setRadius(220);
+      else if (w < 1024) setRadius(310);
+      else if (w < 1280) setRadius(380);
+      else setRadius(420);
     };
     onResize();
     window.addEventListener("resize", onResize);
